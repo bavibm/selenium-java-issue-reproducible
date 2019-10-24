4e4fa6af7da2c9422fe9fcdca429df9cf3fa2bbf
Brandon Villagran <bvillagran@ibm.com>
2019-10-24 17:04:36 -0400

    Added copy dependencies and classpath and main class to jar plugin to pom

--- selenium-issue/src/main/java/module-info.java
+++ selenium-issue/src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module com.example.issue {
     exports com.example.issue;
 
-    requires org.openqa.selenium.chrome;
-    requires org.openqa.selenium.core;
-    requires org.openqa.selenium.firefox;
-    requires org.openqa.selenium.ie;
+//    requires org.openqa.selenium.chrome;
+//    requires org.openqa.selenium.core;
+//    requires org.openqa.selenium.firefox;
+//    requires org.openqa.selenium.ie;
     
-//    requires selenium.api;
-//    requires selenium.chrome.driver;
-//    requires selenium.firefox.driver;
-//    requires selenium.ie.driver;
+    requires selenium.api;
+    requires selenium.chrome.driver;
+    requires selenium.firefox.driver;
+    requires selenium.ie.driver;
 
 }
